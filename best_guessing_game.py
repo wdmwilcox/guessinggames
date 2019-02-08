07f51e33ab92e4e79e33411d03ba074066e42c10
wdmwilcox <46979186+wdmwilcox@users.noreply.github.com>
2019-02-07 18:06:10 -0600

    Add files via upload

--- best_guessing_game.py
+++ best_guessing_game.py
@@ -1,19 +1,22 @@
 import time
 
 def main():
-	numbers = []
-	print('How many numbers do you want to guess from?')
-	amount_of_numbers = int(input(' > '))
 	print('What is your number?')
 	player_number = int(input(' > '))
 	print('Let me guess your number.')
-	time.sleep(1)
+	time.sleep(3)
 	guess = 0
 	while guess != player_number:
+		time.sleep(1)
 		guess = player_number
-	print(f'Is it {guess}?')
-	time.sleep(1)
-	print(f'Yup, that\'s it!')
+		print(f'Is it {guess}?')
+	if guess == player_number:
+		time.sleep(1)
+		print(f'Yup, that\'s it!')
+	else:
+		print('This feature is not implemented.')
+		time.sleep(1)
+		exit(1)
 	exit(0)
 	
 main()
